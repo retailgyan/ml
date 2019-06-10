8fe9e09e78f19074d20e76e72311bf68db609004
shreyabansal25 <44158219+shreyabansal25@users.noreply.github.com>
2019-06-10 14:54:36 +0530

    Update ClothingClassPrediction.py

--- ClothingClassPrediction.py
+++ ClothingClassPrediction.py
@@ -3,7 +3,7 @@ from fastai import *
 from fastai.vision import *
 import sys
 
-path = Path('/data/cloth_categories')
+path = Path('data/cloth_categories')
 app = Flask(__name__)
 
 def predict(imagePath):
@@ -11,7 +11,7 @@ def predict(imagePath):
     classes = ['Blouse', 'Blazer', 'Button-Down', 'Bomber', 'Anorak', 'Tee', 'Tank', 'Top', 'Sweater', 'Flannel', 'Hoodie', 'Cardigan', 'Jacket', 'Henley', 'Poncho', 'Jersey', 'Turtleneck', 'Parka', 'Peacoat', 'Halter', 'Skirt', 'Shorts', 'Jeans', 'Joggers', 'Sweatpants', 'Jeggings', 'Cutoffs', 'Sweatshorts', 'Leggings', 'Culottes', 'Chinos', 'Trunks', 'Sarong', 'Gauchos', 'Jodhpurs', 'Capris', 'Dress', 'Romper', 'Coat', 'Kimono', 'Jumpsuit', 'Robe', 'Caftan', 'Kaftan', 'Coverup', 'Onesie']
     single_img_data = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(),size=150).normalize(imagenet_stats)
     learn = create_cnn(single_img_data, models.resnet34)
-    learn.load('/content/Fine-Grained-Clothing-Classification/data/cloth_categories/models/stage-1_sz-150')
+    learn.load('stage-1_sz-150')
     _, _, losses = learn.predict(open_image(imagePath))
     predictions = sorted(zip(classes, map(float, losses)), key=lambda p: p[1], reverse=True)
     return predictions
